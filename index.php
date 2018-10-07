f9697e7be024d93c75df5aaca73b13bafd0551f9
shorifuluap30 <43937241+shorifuluap30@users.noreply.github.com>
2018-10-08 03:05:51 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>attendance system</title>
+	<link rel="stylesheet" type="text/css" href="inc/bootstrap.min.css">
+	<script type="text/javascript" src="inc/jquery.min.js"></script>
+	<script type="text/javascript" src="inc/bootstrap.min.js"></script>
+	<style >
+body {
+     background: url(vv.jpg)no-repeat;
+    background-size: cover;
+    background-position: center center;
+    background-attachment: fixed;
+    width: 100%;
+    height: 647px;
+}
+
+
+		.head{background: rgba(5,5,4,.3);
+			height: 50px;
+			color: #fff;
+			text-align: center;
+			font-family: 
+		}
+		.head h2{
+			color: white;
+    text-shadow: 2px 3px 4px #fff;
+		}
+		
+	</style>
+</head>
+<body>
+   
+
+      
+      	<div class="head">
+      	  <h2>Online Attendance Management System</h2>
+      		
+      	</div>
+      
+      <div class="container">
+   	<nav class="navbar navbar-default">
+           
+    		<div class="container-fluid">
+    			<div class="navbar-header">
+
+    				<a class="Navbar-brand" href="index.php"></a>
+    			</div>
+    		
+    			  <ul class="nav navbar-nav pull-right">
+    			             
+                        <li><a href="login.php">Faculty login</a></li>
+                          <li><a href="st_login.php">Student Login</a></li>
+                    <li><a href="gr_login.php">Guardian Login</a></li>
+                   
+
+    			    
+              
+    			   
+    			   
+    			  	
+    			  </ul>
+    			</div>	
+    			
+    		</nav>
+            </div>
+    	<div class="img">
+    		
+    	</div>
+    		
+    		
+        
+</body>
+</html>
